d30fbc490b82ea45d0e853148f7cc9885b66364e
Vildar <vildar82@gmail.com>
2019-03-20 16:09:12 +0300

    fix ertyfg

--- AcadLib/Model/Errors/IError.cs
+++ AcadLib/Model/Errors/IError.cs
@@ -10,33 +10,19 @@
     public interface IError : IComparable<IError>, IEquatable<IError>
     {
         Extents3d Extents { get; set; }
-
         bool HasEntity { get; set; }
-
         bool CanShow { get; }
-
         Icon Icon { get; set; }
-
         ErrorStatus Status { get; set; }
-
         ObjectId IdEnt { get; set; }
-
         string Message { get; }
-
         string Group { get; }
-
         string ShortMsg { get; }
-
         object Tag { get; set; }
-
         Matrix3d Trans { get; set; }
-
         List<Entity> Visuals { get; set; }
-
         List<ErrorAddButton> AddButtons { get; set; }
-
         Color Background { get; set; }
-
         void AdditionToMessage(string addMsg);
         IError GetCopy();
         void Show();
