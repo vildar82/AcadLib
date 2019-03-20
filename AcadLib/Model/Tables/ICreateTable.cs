efd0b2ed121aff9897c6b5836d3b439e86a08d4b
Vildar <vildar82@gmail.com>
2019-03-20 15:50:56 +0300

    patch Table Merge 1

--- AcadLib/Model/Tables/ICreateTable.cs
+++ AcadLib/Model/Tables/ICreateTable.cs
@@ -6,13 +6,9 @@
     public interface ICreateTable
     {
         string Layer { get; set; }
-
         int NumColumns { get; set; }
-
         int NumRows { get; set; }
-
         string Title { get; set; }
-
         void CalcRows();
         Table Create();
         void Insert(Table table, Document doc);
